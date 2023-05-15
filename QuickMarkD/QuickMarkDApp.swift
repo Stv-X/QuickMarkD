c5bc38a3a1534621f1616a2c1a84a04a404c18df
Stv.X <stv.x@icloud.com>
2023-05-15 10:25:37 +0800

    Initial Commit

---
+++ QuickMarkD/QuickMarkDApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickMarkDApp.swift
+//  QuickMarkD
+//
+//  Created by 徐嗣苗 on 2023/5/15.
+//
+
+import SwiftUI
+
+@main
+struct QuickMarkDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
